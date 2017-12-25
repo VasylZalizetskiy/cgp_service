fecc63a3625b534f527276bf66e24151a6489e01
Vasyl Zalizetskyi <vasyl.zalizetskyi@gmail.com>
2017-12-25 14:51:05 +0200

    add form constraint

--- app/forms/QueryForm.scala
+++ app/forms/QueryForm.scala
@@ -3,24 +3,38 @@ package forms
 import play.api.data.Form
 import play.api.data.Forms._
 import play.api.data.format.Formats._
+import play.api.data.validation.{Constraint, Invalid, Valid, ValidationError}
 
 object QueryForm {
 
-  val getGoldenCodeQuery = Form(
+  val goldenCodeQuery = Form(
     mapping(
       "value" -> of[Double],
       "precision" -> optional(number)
-    )(GetGoldenCodeQuery.apply)(GetGoldenCodeQuery.unapply)
+    )(GoldenCodeQuery.apply)(GoldenCodeQuery.unapply)
   )
 
-  val getDecimalQuery = Form(
+  val decimalQuery = Form(
     mapping(
-      "code" -> nonEmptyText,
-    )(GetDecimalQuery.apply)(GetDecimalQuery.unapply)
+      "code" -> nonEmptyText.verifying(cgpPattern),
+    )(DecimalQuery.apply)(DecimalQuery.unapply)
   )
 
-  case class GetGoldenCodeQuery(value: Double, precision: Option[Int])
+  val summaryQuery = Form(
+    mapping(
+      "code1" -> nonEmptyText.verifying(cgpPattern),
+      "code2" -> nonEmptyText.verifying(cgpPattern),
+    )(SummaryQuery.apply)(SummaryQuery.unapply)
+  )
 
-  case class GetDecimalQuery(code: String)
+  case class GoldenCodeQuery(value: Double, precision: Option[Int])
+
+  case class DecimalQuery(code: String)
+
+  case class SummaryQuery(code1: String, code2: String)
+
+  def cgpPattern: Constraint[String] = Constraint[String]("constraint.code") { code =>
+    if (code.matches("^[01.]+")) { Valid } else { Invalid(ValidationError("Invalid code", code)) }
+  }
 
 }
